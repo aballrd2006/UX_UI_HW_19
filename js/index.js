4cc4134a3bba38a9fe9e8a8fef9d477ad5e88a70
aballrd2006 <69683473+aballrd2006@users.noreply.github.com>
2021-02-02 10:07:43 -0500

    JS

---
+++ js/index.js
@@ -0,0 +1,14 @@
+$("#research").mouseover(function() {
+    $(this).css("box-shadow", "0 0 11px rgba(33,33,33,.2)");
+    
+});
+
+$("#design").mouseover(function() {
+    $(this).css("box-shadow", "0 0 11px rgba(33,33,33,.2)");
+    
+});
+
+$("#webDev").mouseover(function() {
+    $(this).css("box-shadow", "0 0 11px rgba(33,33,33,.2)");
+    
+});
